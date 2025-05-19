dccd96ee30a823152d3d5ed785b4b1959f036fb3
Marty <martyumlas@gmail.com>
2025-05-19 19:48:43 +0800

    started list

---
+++ Chapter3/list.py
@@ -0,0 +1,11 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(*bicycles)
+print(bicycles[0])
+print(bicycles[1])
+print(bicycles[2])
+print(bicycles[3])
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
+print(bicycles[-4])
