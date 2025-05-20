267f7de7e128d9b01d7827df44965bb599b0bb1b
Marty <martyumlas@gmail.com>
2025-05-20 21:35:05 +0800

    organizing list

---
+++ Chapter3/OrganizingList/cars.py
@@ -0,0 +1,34 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru'] 
+
+# print(cars[0])
+
+# cars.sort()
+
+# print(cars)
+
+# print(cars[0])
+
+# cars.sort(reverse=True)
+
+# print(cars)
+
+# cars.reverse()
+
+# print(cars)
+
+
+print(cars)
+
+# test = cars.sort() #this will return Nonedd
+
+# print(test)
+
+sorted = sorted(cars, reverse=True)
+
+
+print(sorted)
+
+
+
+
+
---
+++ Chapter3/OrganizingList/exercize-3-8.py
@@ -0,0 +1,5 @@
+places_to_visit =['hongkong', 'usa', 'spain', 'france', 'italy']
+
+print(places_to_visit)
+
+print(sorted(places_to_visit))w
