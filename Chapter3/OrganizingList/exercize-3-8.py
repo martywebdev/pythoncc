2ea5fca443779ff857f8450964ff812f881b0a7e
martyumlas <martyumlas@gmail.com>
2025-05-25 21:20:07 +0800

    sorting list

--- Chapter3/OrganizingList/exercize-3-8.py
+++ Chapter3/OrganizingList/exercize-3-8.py
@@ -1,5 +1,23 @@
-places_to_visit =['hongkong', 'usa', 'spain', 'france', 'italy']
+places_to_visit = ['hongkong', 'usa', 'spain', 'france', 'italy']
 
-print(places_to_visit)
+print(f'original list {places_to_visit}')
 
-print(sorted(places_to_visit))w
+
+temp = sorted(places_to_visit)
+
+print(f'sorted list alphabetical temp {temp}')
+
+
+temp_reverse = sorted(places_to_visit, reverse=True)
+
+print(f'sorted list reverse {temp_reverse}')
+
+print(f'original list {places_to_visit}')
+
+places_to_visit.reverse()
+print(f'original list reversed {places_to_visit}')
+places_to_visit.reverse()
+print(f'back to original {places_to_visit}')
+places_to_visit.sort()
+print(f'original list sorted alphabetically {places_to_visit}')
+print(f'reversed alphabetical {sorted(places_to_visit, reverse=True)}')
