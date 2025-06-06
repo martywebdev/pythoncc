12106c937ac38d9c3ea69470ed7c50ab891361f6
martyumlas <martyumlas@gmail.com>
2025-06-06 17:26:42 +0800

    shirt function

---
+++ Chapter8Functions/tshirt.py
@@ -0,0 +1,9 @@
+def make_shirt(size="small", text="I Love New York"):
+    print(f"Making a {size} shirt with the text: '{text}'")
+
+
+make_shirt("large")
+make_shirt("medium", "Python is awesome")
+make_shirt(text="Hello world")
+
+make_shirt(text="hello world", size="large")  
