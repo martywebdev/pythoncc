357abd4e87c09c4e225819ba6a42e997b636e55e
martyumlas <martyumlas@gmail.com>
2025-06-06 17:11:05 +0800

    initial functions

---
+++ Chapter8Functions/greeter.py
@@ -0,0 +1,13 @@
+def display_message():
+    '''This is a function ahahahaha'''
+    print('This is a message of love')
+
+
+display_message()
+
+
+def favorite_book(title):
+    print(f'One of my favorite books is {title.title()}')
+
+
+favorite_book('Alice in wonderland')
